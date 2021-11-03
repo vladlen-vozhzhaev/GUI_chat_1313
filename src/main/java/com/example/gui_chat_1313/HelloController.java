c05ff3cbc497a536251f3440258b1b22584b5d7c
Vladlen <vladlen@vozhzhaev.ru>
2021-11-03 17:54:50 +0300

    Вывод списка пользователей

--- src/main/java/com/example/gui_chat_1313/HelloController.java
+++ src/main/java/com/example/gui_chat_1313/HelloController.java
@@ -9,11 +9,14 @@ import javafx.scene.control.TextField;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HelloController {
     DataOutputStream out;
+    ArrayList<String> usersName = new ArrayList<>();
     @FXML
     Button connectBtn;
     @FXML
@@ -22,6 +25,8 @@ public class HelloController {
     TextField textField;
     @FXML
     TextArea textArea;
+    @FXML
+    TextArea onlineUsersTextArea;
     @FXML
     public void handlerSend(){
         String text = textField.getText();
@@ -40,16 +45,28 @@ public class HelloController {
             Socket socket = new Socket("127.0.0.1", 8178);
             System.out.println("Успешно подключен");
             out = new DataOutputStream(socket.getOutputStream());
-            DataInputStream in = new DataInputStream(socket.getInputStream());
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
                         while (true){
-                            String response = in.readUTF();
-                            textArea.appendText(response+"\n");
+                            String response;
+                            Object object = ois.readObject();
+                            System.out.println(object.getClass());
+                            if(object.getClass().equals(usersName.getClass())){
+                                usersName = (ArrayList<String>) object;
+                                System.out.println(usersName);
+                                onlineUsersTextArea.clear();
+                                for (String userName:usersName) {
+                                    onlineUsersTextArea.appendText(userName+"\n");
+                                }
+                            }else{
+                                response = object.toString();
+                                textArea.appendText(response+"\n");
+                            }
                         }
-                    } catch (IOException e) {
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
 
